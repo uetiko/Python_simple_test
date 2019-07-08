500c8ad33bccc2149065180f56b99be28ac5a019
Angel Barrientos <uetiko@gmail.com>
2019-07-08 14:17:50 -0500

    Added run in main file for binari gap

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 from src.Braces.Application.Syntax import Braces
 from src.Braces.Domain.Syntax import StringStack
+from src.Algorithm.Application.Algorithm import BinaryGapRages
 
 
 input = [
@@ -11,3 +12,8 @@ input = [
 test = Braces(input, StringStack())
 
 print(test.validate())
+
+algorithm = BinaryGapRages(500)
+
+for item in algorithm.run():
+    print(item)
